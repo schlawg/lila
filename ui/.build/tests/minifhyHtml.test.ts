9c6116fc6a8c6f2c5341cc5bd6e68251fdbd7a0b
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2025-01-11 22:53:07 -0500

    test minifying string

--- ui/.build/tests/minifhyHtml.test.ts
+++ ui/.build/tests/minifhyHtml.test.ts
@@ -1,17 +1,23 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { minifyHtml } from "../src/parse";
 
-test("minifyHtml", () => {
-    const html = `
-        <html>
-        <head>
-            <title>Test</title>
-        </head>
-        <body>
-            <h1>Test</h1>
-        </body>
-        </html>
-    `;
-    const minified = minifyHtml(html);
-    expect(minified).toBe("<html><head><title>Test</title></head><body><h1>Test</h1></body></html>");
+describe('minify', () => {
+    test('string', () => {
+        expect(minifyHtml('a       b')).toBe('a b');
+        expect(minifyHtml('tab\ttab')).toBe('tab tab');
+    })
+    test("html", () => {
+        const html = `
+            <html>
+            <head>
+                <title>Test</title>
+            </head>
+            <body>
+                <h1>Test</h1>
+            </body>
+            </html>
+        `;
+        const minified = minifyHtml(html);
+        expect(minified).toBe("<html><head><title>Test</title></head><body><h1>Test</h1></body></html>");
+    });
 });
